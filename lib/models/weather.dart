92f031275f5e808bfe041353a8b3271ac8172928
Rahul sharma <92357155+Rahulqqq@users.noreply.github.com>
2022-12-16 22:20:18 +0530

    update in weather
    nothing much

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 
 class Weather {
   final String cityName;
-  final double temperature;
+  final double temperatur;
 
   Weather({required this.cityName, required this.temperature});
 }
